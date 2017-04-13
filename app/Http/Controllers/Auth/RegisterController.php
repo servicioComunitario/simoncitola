03304344392fb773d68645c0beef8ac943c784e8
stalinscj <ssanchez684@gmail.com>
2017-04-12 20:41:50 -0400

    Cambio en l validador de usuarios

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'email' => 'required|email|max:255|unique:users',
+            'email' => 'required|email|max:255|unique:usuarios',
             'password' => 'required|min:6|confirmed',
         ]);
     }
